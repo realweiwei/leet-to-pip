f04c06ed12a092b8b8307a2681f6bafab3a9e660
realweiwei <weiwangubc1@gmail.com>
2025-02-13 23:01:19 -0800

    noice

---
+++ week1/49.group-anagrams.py
@@ -0,0 +1,78 @@
+#
+# @lc app=leetcode id=49 lang=python3
+#
+# [49] Group Anagrams
+#
+# https://leetcode.com/problems/group-anagrams/description/
+#
+# algorithms
+# Medium (70.19%)
+# Likes:    20089
+# Dislikes: 667
+# Total Accepted:    3.5M
+# Total Submissions: 5M
+# Testcase Example:  '["eat","tea","tan","ate","nat","bat"]'
+#
+# Given an array of strings strs, group the anagrams together. You can return
+# the answer in any order.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: strs = ["eat","tea","tan","ate","nat","bat"]
+# 
+# Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+# 
+# Explanation:
+# 
+# 
+# There is no string in strs that can be rearranged to form "bat".
+# The strings "nat" and "tan" are anagrams as they can be rearranged to form
+# each other.
+# The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to
+# form each other.
+# 
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: strs = [""]
+# 
+# Output: [[""]]
+# 
+# 
+# Example 3:
+# 
+# 
+# Input: strs = ["a"]
+# 
+# Output: [["a"]]
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# 1 <= strs.length <= 10^4
+# 0 <= strs[i].length <= 100
+# strs[i] consists of lowercase English letters.
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dict_ = {}
+        for word in strs:
+            sorted_word = tuple(sorted(word))
+            if sorted_word not in dict_:
+                dict_[sorted_word] = [word]
+            else:
+                dict_[sorted_word].append(word)
+        return list(dict_.values())
+        
+# @lc code=end
+
