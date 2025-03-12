09cc03536161099b2e5178d1f2914f3ceb1365b7
realweiwei <weiwangubc1@gmail.com>
2025-03-12 16:15:55 -0700

    good for you jeff

---
+++ week3/125.valid-palindrome.java
@@ -0,0 +1,87 @@
+/*
+ * @lc app=leetcode id=125 lang=java
+ *
+ * [125] Valid Palindrome
+ *
+ * https://leetcode.com/problems/valid-palindrome/description/
+ *
+ * algorithms
+ * Easy (50.08%)
+ * Likes:    10073
+ * Dislikes: 8510
+ * Total Accepted:    3.9M
+ * Total Submissions: 7.8M
+ * Testcase Example:  '"A man, a plan, a canal: Panama"'
+ *
+ * A phrase is a palindrome if, after converting all uppercase letters into
+ * lowercase letters and removing all non-alphanumeric characters, it reads the
+ * same forward and backward. Alphanumeric characters include letters and
+ * numbers.
+ * 
+ * Given a string s, return true if it is a palindrome, or false otherwise.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: s = "A man, a plan, a canal: Panama"
+ * Output: true
+ * Explanation: "amanaplanacanalpanama" is a palindrome.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: s = "race a car"
+ * Output: false
+ * Explanation: "raceacar" is not a palindrome.
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: s = " "
+ * Output: true
+ * Explanation: s is an empty string "" after removing non-alphanumeric
+ * characters.
+ * Since an empty string reads the same forward and backward, it is a
+ * palindrome.
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= s.length <= 2 * 10^5
+ * s consists only of printable ASCII characters.
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            while (!Character.isLetterOrDigit(s.charAt(i)) && i < s.length() - 1) {
+                i++;
+            }
+            while (!Character.isLetterOrDigit(s.charAt(j)) && j > 0) {
+                j--;
+            }
+            if (i >= j) {
+                return true;
+            }
+            if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) {
+                return false;
+            } else {
+                i++;
+                j--;
+            }
+        }
+        return true;
+    }
+}
+// @lc code=end
+
