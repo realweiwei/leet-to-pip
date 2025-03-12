60ea93ca86b62b8f8c483b124cc17778ee13ea72
realweiwei <weiwangubc1@gmail.com>
2025-03-12 10:07:58 -0700

    well

---
+++ week3/271.encode-and-decode-strings_1.java
@@ -0,0 +1,129 @@
+/*
+ * @lc app=leetcode id=271 lang=java
+ *
+ * [271] Encode and Decode Strings
+ *
+ * https://leetcode.com/problems/encode-and-decode-strings/description/
+ *
+ * algorithms
+ * Medium (48.89%)
+ * Likes:    1513
+ * Dislikes: 436
+ * Total Accepted:    244.8K
+ * Total Submissions: 499.6K
+ * Testcase Example:  '["Hello","World"]'
+ *
+ * Design an algorithm to encode a list of strings to a string. The encoded
+ * string is then sent over the network and is decoded back to the original
+ * list of strings.
+ * 
+ * Machine 1 (sender) has the function:
+ * 
+ * 
+ * string encode(vector<string> strs) {
+ * ⁠ // ... your code
+ * ⁠ return encoded_string;
+ * }
+ * Machine 2 (receiver) has the function:
+ * 
+ * 
+ * vector<string> decode(string s) {
+ * ⁠ //... your code
+ * ⁠ return strs;
+ * }
+ * 
+ * 
+ * So Machine 1 does:
+ * 
+ * 
+ * string encoded_string = encode(strs);
+ * 
+ * 
+ * and Machine 2 does:
+ * 
+ * 
+ * vector<string> strs2 = decode(encoded_string);
+ * 
+ * 
+ * strs2 in Machine 2 should be the same as strs in Machine 1.
+ * 
+ * Implement the encode and decode methods.
+ * 
+ * You are not allowed to solve the problem using any serialize methods (such
+ * as eval).
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: dummy_input = ["Hello","World"]
+ * Output: ["Hello","World"]
+ * Explanation:
+ * Machine 1:
+ * Codec encoder = new Codec();
+ * String msg = encoder.encode(strs);
+ * Machine 1 ---msg---> Machine 2
+ * 
+ * Machine 2:
+ * Codec decoder = new Codec();
+ * String[] strs = decoder.decode(msg);
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: dummy_input = [""]
+ * Output: [""]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= strs.length <= 200
+ * 0 <= strs[i].length <= 200
+ * strs[i] contains any possible characters out of 256 valid ASCII
+ * characters.
+ * 
+ * 
+ * 
+ * Follow up: Could you write a generalized algorithm to work on any possible
+ * set of characters?
+ * 
+ */
+
+// @lc code=start
+public class Codec {
+    private int[] len = {};
+
+    // Encodes a list of strings to a single string.
+    public String encode(List<String> strs) {
+        String res = String.join("", strs);
+        this.len = new int[strs.size()];
+        for (int i = 0; i < strs.size(); i++) {
+            len[i] = strs.get(i).length();
+        }
+        return res;
+    }
+
+    // Decodes a single string to a list of strings.
+    public List<String> decode(String s) {
+        List<String> res = new ArrayList<>();
+        int i = 0;
+        for (int l : len) {
+            String curr = s.substring(i, i + l);
+            if (l == 0) {
+                curr = "";
+            }
+            res.add(curr);
+            i = i + l;
+        }
+        return(res);
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(strs));
+// @lc code=end
+
